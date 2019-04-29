8f53c7153e94b60880cd6064b3af9a204c6da48e
Muhammed Ali Yavuzcan <mayavuzcan@gmail.com>
2019-04-29 03:35:20 +0300

    Hata Giderme

---
+++ src/main/java/com/firat/streetBall/StreetBallApplication.java
@@ -0,0 +1,14 @@
+package com.firat.streetBall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class StreetBallApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StreetBallApplication.class, args);
+	}
+	
+}
